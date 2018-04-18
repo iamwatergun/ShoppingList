949c7837c29858ea87361b2e8bb8ee28959df910
nrotter-tgm <nrotter@student.tgm.ac.at>
2018-04-18 11:39:17 +0200

    first commit

---
+++ app/src/main/java/firebase/com/shoppinglistapp/Task.java
@@ -0,0 +1,12 @@
+package firebase.com.shoppinglistapp;
+
+public class Task {
+    private String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
